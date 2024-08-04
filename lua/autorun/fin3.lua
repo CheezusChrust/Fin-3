2f70e55ee62adedea63d887442f4d3afe0f19b09
Matt <rcheeze1337@gmail.com>
2024-08-04 14:32:19 -0400

    Fix English localization file not being sent to clients

--- lua/autorun/fin3.lua
+++ lua/autorun/fin3.lua
@@ -8,6 +8,7 @@ if SERVER then
     AddCSLuaFile("fin3/sh_fin3_globals.lua")
     AddCSLuaFile("fin3/sh_fin3_util.lua")
     AddCSLuaFile("fin3/sh_fin3_models.lua")
+    resource.AddFile("resource/localization/en/fin3.properties")
 else
     include("fin3/cl_fin3_hud.lua")
 end
